2cdfceedf89f66a08b0cd5b57b35553fa092c452
Drew Springfield <drew@robosim.com>
2025-02-03 21:36:07 -0800

    update

--- scripts/build_full_trainer.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-cd ../ai
-docker build -t pybase . -f ./Dockerfile --no-cache
-docker build -t plo_trainer . -f ./Dockerfile.train --no-cache
-cd ../scripts
--- scripts/build_trainer.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-cd ../ai
-docker build -t plo_trainer . -f ./Dockerfile.train --no-cache
-cd ../scripts
